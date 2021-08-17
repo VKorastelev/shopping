2fe5bdb9eb232fd249af672e41a4a8601b9b613c
VKorastelev <86928211+VKorastelev@users.noreply.github.com>
2021-08-17 15:48:51 +0700

    1.1

--- randinrange.c
+++ randinrange.c
@@ -3,40 +3,6 @@
 #include <sys/time.h>
 #include "randinrange.h"
 
-/*
-int main()
-{
-    int min_num = 0;
-    int max_num = 5;
-    int rand_num;
-
-    rand_init();
-
-    for (int i = 0; i < 1000000; i++)
-    {
-        rand_num = rand_in_range(min_num, max_num);
-        if (rand_num < min_num || rand_num > max_num)
-        {
-            printf("Вышли за границу интервала случайных чисел [%d, %d]\n", min_num,
-                    max_num);
-            exit(EXIT_FAILURE);
-        }
-
-        if (min_num == rand_num || max_num == rand_num)
-        {
-            printf("Граница интервала [%d, %d], rand_num = %d\n", min_num, max_num,
-                    rand_num);
-        }
-        else
-        {
-            printf("rand_num = %d\n", rand_num);
-        }
-    }
-
-    return 0;
-}
-*/
-
 void rand_init()
 {
     struct timeval strtime;
